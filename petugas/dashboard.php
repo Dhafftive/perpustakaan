77db5880f1364b4c336ed68a557f80344f475e61
dhafftive <didaarfiana05@gmail.com>
2024-01-19 23:09:35 +0700

    trial commit

---
+++ petugas/dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="sidebar">
+        <nav>
+            <ul>
+                <li>Dashboard</li>
+                <li>Peminjaman</li>
+                <li>Koleksi</li>
+                <li>Petugas</li>
+                <li></li>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>
