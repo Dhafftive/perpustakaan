99064eb85c478b93e864f645c4d9c6c7b9801eeb
dhafftive <didaarfiana05@gmail.com>
2024-01-24 18:19:12 +0700

    login script changes

--- process_login.php
+++ process_login.php
@@ -2,15 +2,10 @@
 session_start();
 include "koneksi.php"; // Sesuaikan dengan nama file koneksi ke database
 
-$response = array();
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $usernameOrEmail = $_POST["username"];
     $password = $_POST["password"];
 
-    // Lakukan validasi atau filter input sesuai kebutuhan
-
-    // Cek apakah username atau email ada di dalam database
     $query = "SELECT * FROM user WHERE (username = '$usernameOrEmail' OR email = '$usernameOrEmail')";
     $result = mysqli_query($koneksi, $query);
 
@@ -18,7 +13,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (mysqli_num_rows($result) > 0) {
             $row = mysqli_fetch_assoc($result);
             if (password_verify($password, $row["password"])) {
-                // Password benar, buat sesi dan respon berhasil
                 $_SESSION["user_id"] = $row["userID"];
                 $_SESSION["username"] = $row["username"];
                 $_SESSION["email"] = $row["email"];
@@ -28,36 +22,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $_SESSION["acces_level"] = $row["acces_level"];
                 $_SESSION["no_hp"] = $row["no_hp"];
                 
-                // Set waktu ekspirasi sesi (5 jam)
                 $_SESSION["expire_time"] = time() + 5 * 60 * 60;
 
-                $response["success"] = true;
-                $response["message"] = "Login berhasil";
-                $response["acces_level"] = $row["acces_level"];
-            } else {
+                $logs = "INSERT INTO c_logs (detail_histori) VALUES ('User bernama " . $_SESSION["username"] . " telah login')";
+                // Redirect ke halaman sesuai dengan acces_level
+                if (mysqli_query($koneksi, $logs)) {
+                    // Redirect ke halaman sesuai dengan acces_level
+                    switch ($_SESSION["acces_level"]) {
+                        case "admin":
+                            header("Location: admin.php");
+                            exit();
+                        case "petugas":
+                            header("Location: petugas.php");
+                            exit();
+                        case "peminjam":
+                            header("Location: peminjam.php");
+                            exit();
+                        default:
+                            // Jika acces_level tidak valid, arahkan ke halaman login
+                            header("Location: login.php");
+                            exit();
+                        }
+                } else {
                 // Password salah
-                $response["success"] = false;
-                $response["message"] = "Password Anda salah";
+                echo "Password salah";
+                }
+            } else {
+                // Akun tidak ditemukan
+                echo "Akun ini tidak ditemukan";
             }
         } else {
-            // Akun tidak ditemukan
-            $response["success"] = false;
-            if (empty($usernameOrEmail) || empty($password)) {
-                $response["message"] = "Username atau email dan password harus diisi";
-            } else {
-                $response["message"] = "Username atau email Anda salah";
-            }
+            // Kesalahan query
+            echo "Terjadi kesalahan saat melakukan query";
         }
-    } else {
-        // Kesalahan query
-        $response["success"] = false;
-        $response["message"] = "Terjadi kesalahan saat melakukan query";
-    }
-    
-    mysqli_close($koneksi);
-
-    // Memberikan respon dalam format JSON
-    header('Content-Type: application/json');
-    echo json_encode($response);
+        mysqli_close($koneksi);
+    }   
 }
 ?>
