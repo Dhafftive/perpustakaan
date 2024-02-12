325bc61b40da5f98222a24faf40e7f738033a37e
dhafftive <didaarfiana05@gmail.com>
2024-02-12 23:41:41 +0700

    [Feat] Logs

--- admin/function/cek_login.php
+++ admin/function/cek_login.php
@@ -10,6 +10,10 @@ if (!isset($_SESSION["user_id"])) {
 
 // Periksa waktu kadaluarsa
 if (isset($_SESSION["expire_time"]) && time() > $_SESSION["expire_time"]) {
+    // Masukkan ke dalam tabel c_logs bahwa user telah logout karena sesi berakhir
+    $logout_log = "INSERT INTO c_logs (detail_histori) VALUES ('User ".$_SESSION["username"]." telah logout karena sesi berakhir')";
+    mysqli_query($koneksi, $logout_log);
+
     // Jika waktu sesi sudah habis, hapus semua variabel sesi dan redirect ke halaman login
     session_unset();
     session_destroy();
